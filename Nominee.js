5fe51a2a25896ebe0ab4e2e51fb85f256b9cb595
usersblock <thanhphuocly@hotmail.com>
2016-11-16 22:43:50 -0500

    Update Nominee.js

--- Nominee.js
+++ Nominee.js
@@ -3,6 +3,9 @@ this.firstname=first;
 this.lastname=last;
 this.party=party;
 this.state=state;
+this.liberal=0;
+this.moderate=0;
+this.conservative=0;
 this.charisma=stats[0];
 this.appearance=stats[1];
 this.credibility=stats[2]
@@ -13,4 +16,38 @@ this.minorityappeal=stats[6];
 this.religious=stats[7];
 this.fundraisingability=stats[8];
 this.stamina=stats[9];
-this.funds=stats[10];}
+this.funds=stats[10];
+
+this.getName= function(){
+return(this.firstname+" "+this.lastname);
+}
+
+this.getParty=function(){
+return this.party;
+}
+
+this.getState=function(){
+return this.state;
+}
+
+this.getLiberalAppeal(){
+return this.liberal;
+}
+
+this.changeLiberalAppeal=function(change){
+this.liberal=this.liberal+change;  
+  
+this.getModerateAppeal(){
+return this.moderate;
+}
+this.changeModerateAppeal=function(change){
+this.moderate=this.moderate+change;
+}    
+this.getConservativeAppeal(){
+return this.conservative;
+}
+
+this.changeConservativeAppeal=function(change){
+this.conservative=this.conservative+change;
+}  
+}
