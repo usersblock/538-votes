bb45f76f72242051419aad32277f49e865b24544
usersblock <thanhphuocly@hotmail.com>
2016-11-16 22:52:31 -0500

    Update States.js

--- States.js
+++ States.js
@@ -8,4 +8,44 @@ this.conservative=stats[2];
 this.democrat=stats[3];
 this.independent=stats[4];
 this.republican=stats[5];
+  
+this.getName=function(){
+return this.name;
+}
+
+this.getPoints=function(){
+return this.points;
+}
+
+this.getWealth=function(){
+return this.wealth;
+}
+
+this.changeWealth=function(change){
+this.wealth=this.wealth+change;
+}
+
+this.getLiberal=function(){
+return this.liberal;
+}
+
+this.getModerate=function(){
+return this.moderate;
+}
+
+this.getConservative=function(){
+return this.conservative;
+}
+
+this.getDemocrat=function(){
+return this.democrat;
+}
+
+this.getIndependent=function(){
+return this.independent;
+}
+
+this.getRepublican=function(){
+return this.republican;
+}
 }
