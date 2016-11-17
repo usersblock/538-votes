635b6c10922dd91eb3a3933950d800cc3e66d851
usersblock <thanhphuocly@hotmail.com>
2016-11-16 22:47:58 -0500

    Update Issues.js

--- Issues.js
+++ Issues.js
@@ -7,4 +7,36 @@ this.dopinion=dopinion;
 this.dimportance=dimportance;
 this.iopinion=iopinion;
 this.iimportance=iimportance;
+  
+this.getName=function(){
+return this.name;
+}
+  
+this.getDescription=function(){
+return this.description;
+}  
+  
+this.getRepublicanImportance=function(){
+return this.rimportance;
+}
+
+this.getRepublicanOpinion=function(){
+return this.ropinion
+}
+
+this.getDemocraticImportance=function(){
+return this.dimportance
+}
+
+this.getDemocraticOpinion=function(){
+return this.dopinion
+}
+
+this.getIndependentOpinion=function(){
+return this.iopinion
+}
+
+this.getIndependentImportane=function(){
+return this.iimportance
+}
 }
