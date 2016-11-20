68bd678ee2464b39e56b155f0bed3ad0110ef912
usersblock <thanhphuocly@hotmail.com>
2016-11-19 20:24:48 -0500

    Labeling
    Added comments to make Nominee.js more readable

--- Nominee.js
+++ Nominee.js
@@ -1,3 +1,15 @@
+/*stats-An array of statistics that will be derived from a single line of NomineesInfo.csv.
+Input:This constructor will take stats as a parameter.
+Output:Will create an object with the variables of 
+(firstname,lastname,party,state,liberal,
+moderate,conservative,charisma,appearance,
+credibility,experience,intelligence,mediabias,
+minorityappeal,religious,fundraisingability,
+stamina,funds)
+*/
+
+
+
 function person(stats){
 this.firstname=stats[0];
 this.lastname=stats[1];
@@ -17,36 +29,45 @@ this.religious=stats[11];
 this.fundraisingability=stats[12];
 this.stamina=stats[13];
 this.funds=stats[14];
-
+//Output:Will return the firstname and lastname in String form.
 this.getName= function(){
 return(this.firstname+" "+this.lastname);
 }
-
+//Output:Will return the party in String form.
 this.getParty=function(){
 return this.party;
 }
 
+//Output:Will return the nominee's home state in String form.
 this.getState=function(){
 return this.state;
 }
 
+//Output:Will return the liberal appeal in integer form.
 this.getLiberalAppeal(){
 return this.liberal;
 }
-
+/*Input:Will take an integer change.
+Output:Will change the liberal variable by "change" amount.*/
 this.changeLiberalAppeal=function(change){
 this.liberal=this.liberal+change;  
-  
+
+//Output:Will return the moderate appeal in integer form.  
 this.getModerateAppeal(){
 return this.moderate;
 }
+/*Input:Will take an integer change.
+Output:Will change the moderate variable by "change" amount.*/  
 this.changeModerateAppeal=function(change){
 this.moderate=this.moderate+change;
-}    
+}
+
+//Output:Will return the conservative appeal in integer form.
 this.getConservativeAppeal(){
 return this.conservative;
 }
-
+/*Input:Will take an integer change.
+Output:Will change the conservative variable by "change" amount.*/
 this.changeConservativeAppeal=function(change){
 this.conservative=this.conservative+change;
 }  
