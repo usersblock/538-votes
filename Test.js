06c2a690d32fd692848a173449acefb7fd771378
usersblock <thanhphuocly@hotmail.com>
2016-11-22 13:04:39 -0500

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,7 @@
+function CreateNominee(stats){
+var Nom=new Nominee(stats);
+var WordSpace=document.getElementById("DATATEST1");
+WordSpace=Nom.getName();
+}
+
+CreateNominee(["Hillary","Clinton",1,1,1,,1,1,1,1,1,1,1,1,1,1]);
